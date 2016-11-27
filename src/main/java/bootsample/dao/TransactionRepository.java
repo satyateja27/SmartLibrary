8f2f3d8b1ed46ad339033a8b175208ee67f2f212
tiyakukreja <tiyakukreja@gmail.com>
2016-11-27 00:36:04 -0800

    Added namedqueries

--- src/main/java/bootsample/dao/TransactionRepository.java
+++ src/main/java/bootsample/dao/TransactionRepository.java
@@ -1,9 +1,28 @@
 package bootsample.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import bootsample.model.Transaction;
 
-public interface TransactionRepository extends CrudRepository<Transaction, Integer>{
+public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
+
+	@Query(value = "SELECT count(*) FROM smartlibrary.transaction where cast(start_date as date)=?2 and user_user_id=?1", nativeQuery = true)
+	int countPerDay(int id, Date date);
+
+	@Query(value = "SELECT count(*) FROM smartlibrary.transaction where user_user_id=? and return_flag=0", nativeQuery = true)
+	int countPerUser(int id);
+
+	@Query(value = "SELECT * FROM smartlibrary.transaction where return_flag=0 and user_user_id=?;", nativeQuery = true)
+	List<Transaction> findIssuedBooksPerUser(int id);
+
+	Transaction findByTransactionId(int tranid);
+
+	@Query(value = "UPDATE smartlibrary.transaction SET due=?1, return_date=?2, return_flag=1 WHERE transaction_id=?3'", nativeQuery = true)
+	void returnBook(int due, Date date, int tranid);
 
 }
+
